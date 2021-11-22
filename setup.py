ae8906cf5e80bf21600e3a975b04a0f6aec78648
Joe Fahnestock <joe.fahnestock@odb.org>
2021-11-22 11:39:09 -0500

    Name 1.3.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
     readme = f.read()
 
 setup(
-    version='1.3.4',
+    version='1.3.2',
     name='FuelSDKWrapper',
     description='Simplify and enhance the FuelSDK for Salesforce Marketing Cloud (ExactTarget)',
     long_description=readme,
